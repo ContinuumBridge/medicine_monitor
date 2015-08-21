4dfb92d47ccf19d5a0d10873257750529b2de9cf
PeterClaydon <peter.claydon@continuumbridge.com>
2015-08-21 14:30:55 +0100

    First tested version

--- medicine.py
+++ medicine.py
@@ -9,30 +9,29 @@ config = {
     "medicine": True,
     "medicine_name": "My_Medicine",
     "time_slots": [
-        [
-            {
-                "name": "Morning",
-                "start": "07:00", 
-                "end": "08:00"
-            },
-            {
-                "name": "Evening",
-                "start": "21:00", 
-                "end": "22:00"
-            }
-        ]
+        {
+            "name": "Morning",
+            "start": "07:00", 
+            "end": "08:00"
+        },
+        {
+            "name": "Evening",
+            "start": "18:00", 
+            "end": "22:00"
+        }
     ],
     "reminders": True,
-    "alerts": True,
+    "remiinder_time": 600,
     "accel_min_change": 0.2,  # The min change in g on any axis to indicate that medicine is being taken
-    "ignore_time": 120        # Movement within this interval will not set another alert
+    "ignore_time": 30,        # Movement within this interval will not set another alert
+    "data_send_delay": 1
 }
 
 import sys
 import os.path
 import time
 from cbcommslib import CbApp, CbClient
-from cbutils import betweenTimes
+from cbutils import betweenTimes, hourMin2Epoch, nicetime
 from cbconfig import *
 import json
 from twisted.internet import reactor
@@ -41,57 +40,88 @@ CONFIG_FILE                       = CB_CONFIG_DIR + "medicine_monitor.config"
 CID                               = "CID164"  # Client ID
 
 class Medicine():
-    def __init__(self, bridge_id):
+    def __init__(self, bridge_id, name):
         self.bridge_id = bridge_id
+        self.name = name
         self.s = []
         self.waiting = False
+        self.starting = 0
         self.lastValues = [0.0, 0.0, 0.0]
-        self.lastTime = time.time()
+        self.lastTime = 0
         self.taken = False
+        self.lastReminderTime = 0
+        reactor.callLater(1, self.monitor)
 
     def onChange(self, timeStamp, values):
+        #self.cbLog("debug", "onChange. values: " + str(values))
         try:
+            if self.starting < 3:
+                self.lastValues = values
+                self.starting += 1
+                return
             moved = False
-            for v in values:
+            for v in range(0,3):
                 if abs(values[v] - self.lastValues[v]) > config["accel_min_change"]:
                     moved = True
             if moved:
                 if timeStamp - self.lastTime > config["ignore_time"]:
-                    self.lastTime = timeStamp:
+                    self.lastTime = timeStamp
                     self.taken = True
-            self.lastValues = value:
+            self.lastValues = values
         except Exception as ex:
             self.cbLog("warning", "medicine onChange encountered problems. Exception: " + str(type(ex)) + str(ex.args))
 
     def monitor(self):
-        # Called every 10 seconds
+        # Called every 5 seconds
+        #self.cbLog("debug", "monitor. taken: " + str(self.taken))
         try:
             if self.taken:
                 self.taken = False
-                slotName = None
+                inSlot = False
                 for s in config["time_slots"]:
                     if betweenTimes(self.lastTime, s["start"], s["end"]):
-                        slotName = s["name"]
+                        inSlot = True
                         break
-                name = config["medicine_name"].replace(" ", "_")
-                if slotName:
+                joinedName = self.name.replace(" ", "_")
+                if inSlot:
                     values = {
-                        "name": self.bridge_id + "/" + name + "/" + in_slot,
-                        "points": [[int(timeStamp*1000), 1]]
+                        "name": self.bridge_id + "/" + joinedName + "/" + "in_slot",
+                        "points": [[int(self.lastTime*1000), 1]]
                     }
                 else:
                     values = {
-                        "name": self.bridge_id + "/" + name + "/" + out_slot,
-                        "points": [[int(timeStamp*1000), 1]]
+                        "name": self.bridge_id + "/" + joinedName + "/" + "out_slot",
+                        "points": [[int(self.lastTime*1000), 1]]
+                    }
                 self.storeValues(values)
         except Exception as ex:
             self.cbLog("warning", "medicine monitor encountered problems in taken. Exception: " + str(type(ex)) + str(ex.args))
+        try:
+            if config["reminders"]:
+                now = time.time()
+                if now - self.lastReminderTime > config["reminder_time"] + 10:
+                    for s in config["time_slots"]:
+                        if betweenTimes(now, s["start"], s["end"]):
+                            if not betweenTimes(self.lastTime, s["start"], s["end"]):
+                                epochEnd = hourMin2Epoch(s["end"])
+                                if epochEnd - now < config["reminder_time"]:
+                                    msg = {"m": "alert",
+                                           "a": "Remember to take your " + self.name + " by " + nicetime(epochEnd)[:5],
+                                           "t": now
+                                          }
+                                    self.client.send(msg)
+                                    self.cbLog("debug", "msg send to client: " + str(json.dumps(msg, indent=4)))
+                                    self.lastReminderTime = now
+                                    break
+        except Exception as ex:
+            self.cbLog("warning", "medicine monitor encountered problems in reminders. Exception: " + str(type(ex)) + str(ex.args))
+        reactor.callLater(5, self.monitor)
 
     def sendValues(self):
         msg = {"m": "data",
                "d": self.s
                }
-        #self.cbLog("debug", "sendValues. Sending: " + str(json.dumps(msg, indent=4)))
+        self.cbLog("debug", "sendValues. Sending: " + str(json.dumps(msg, indent=4)))
         self.client.send(msg)
         self.s = []
         self.waiting = False
@@ -108,8 +138,8 @@ class App(CbApp):
         self.state = "stopped"
         self.status = "ok"
         self.devices = []
-        self.devServices = [] 
         self.idToName = {} 
+        self.medicine = {} 
         #CbApp.__init__ MUST be called
         CbApp.__init__(self, argv)
 
@@ -153,26 +183,29 @@ class App(CbApp):
 
     def onAdaptorData(self, message):
         #self.cbLog("debug", "onAdaptorData, message: " + str(json.dumps(message, indent=4)))
-        if message["characteristic"] == "acceleration":
-            self.medicine.onChange(message["timeStamp"], message["data"])
+        try:
+            if message["characteristic"] == "acceleration":
+                self.medicine[message["id"]].onChange(message["timeStamp"], [message["data"]["x"], message["data"]["y"], message["data"]["z"]])
+        except Exception as ex:
+            self.cbLog("warning", "onAdaptorData problem. Exception: " + str(type(ex)) + str(ex.args))
 
     def onAdaptorService(self, message):
         #self.cbLog("debug", "onAdaptorService, message: " + str(json.dumps(message, indent=4)))
-        if self.state == "starting":
-            self.setState("running")
-        self.devServices.append(message)
-        serviceReq = []
-        power = False
-        biinary = False
-        for p in message["service"]:
-            if p["characteristic"] == "acceleration":
-                self.medicine.gotSensor = True
-            serviceReq.append({"characteristic": "acceleration", "interval": 2})
-        msg = {"id": self.id,
-               "request": "service",
-               "service": serviceReq}
-        self.sendMessage(msg, message["id"])
-        self.cbLog("debug", "onAdaptorService, response: " + str(json.dumps(msg, indent=4)))
+        try:
+            if self.state == "starting":
+                self.setState("running")
+            serviceReq = []
+            for p in message["service"]:
+                if p["characteristic"] == "acceleration":
+                    self.medicine[message["id"]].gotSensor = True
+                    serviceReq.append({"characteristic": "acceleration", "interval": 2})
+            msg = {"id": self.id,
+                   "request": "service",
+                   "service": serviceReq}
+            self.sendMessage(msg, message["id"])
+            self.cbLog("debug", "onAdaptorService, response: " + str(json.dumps(msg, indent=4)))
+        except Exception as ex:
+            self.cbLog("warning", "onAdaptorServices problem. Exception: " + str(type(ex)) + str(ex.args))
 
     def readLocalConfig(self):
         global config
@@ -207,10 +240,10 @@ class App(CbApp):
         self.client.onClientMessage = self.onClientMessage
         self.client.sendMessage = self.sendMessage
         self.client.cbLog = self.cbLog
-        self.medicine = medicine(self.bridge_id)
-        self.medicine.cbLog = self.cbLog
-        self.medicine.client = self.client
-        self.medicine.setNames(idToName2)
+        for a in idToName2:
+            self.medicine[a] = Medicine(self.bridge_id, idToName2[a])
+            self.medicine[a].cbLog = self.cbLog
+            self.medicine[a].client = self.client
         self.setState("starting")
 
 if __name__ == '__main__':
